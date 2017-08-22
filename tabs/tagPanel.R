6472ec2a3f9e252291839ffe9960c8eb185dcee2
Nick Strayer <nick@Nicks-MacBook-Pro.local>
2017-08-22 18:28:11 -0400

    added a small snippet about how to tag

--- tabs/tagPanel.R
+++ tabs/tagPanel.R
@@ -1,5 +1,12 @@
 tagPanel <- function(){
   div(
+    br(),
+    div(
+      "Tag your data by clicking and dragging your cursor over a region of your plotted data. A small dialog will appear that you can enter your",
+      "activity label into. The aggregate counts of your tags will appear above for your reference. The more tags for a given activity we have the better",
+      "our algorithm will be able to predict it in the future!"
+    ),
+    hr(),
     div(id = "tag_downloading_animation",
         div(class = "centered", h1("We're fetching your data!")),
         div(class = "centered", helixLoader())
